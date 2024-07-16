74244705a41447a36e7215493288fa4c828ec92d
Gnomeslayer <141653739+Samplerih@users.noreply.github.com>
2024-07-16 14:51:48 +0530

    Update

---
+++ 7.py
@@ -0,0 +1,9 @@
+print('program to find something interesting ')
+
+i = int(input('enter the number : '))
+a=i
+for i in range(i):
+	a -= 1
+	print(i*a )
+print('...............')
+print('mirror world :)')
